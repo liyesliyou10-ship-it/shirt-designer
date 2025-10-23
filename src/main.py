a716383b347ee5e9983463f36badd51ed9471914
liyesliyou10-ship-it <liyesliyou10@gmail.com>
2025-10-23 15:13:32 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+        <head>
+            <title>Shirt Designer</title>
+        </head>
+        <body style="text-align: center; font-family: sans-serif;">
+            <h1>Welcome to the Shirt Designer!</h1>
+            <p>This site is powered by <b>FastAPI</b> and hosted on <b>Wasmer</b>.</p>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
